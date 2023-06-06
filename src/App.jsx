ad1e8192be31426364ef49d404c3e922d7e89307
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-06-06 12:00:54 +0100

    remove firebase, use local file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import { useState, useEffect } from 'react'
-import firebase from './firebase'
 
 import './App.css'
 
-
-// setup and link to firestore db
-const firebaseConfig = {
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
-}
-
-firebase.initializeApp(firebaseConfig)
 
 function App() {
 
   const [ todos, setTodos ] = useState([])
 
   useEffect(() => {
-    const db = firebase.firestore()
-    const data = db.getDatabase('todos')
+    const getTodos = async () => {
+      const response = await fetch('./todos.json')
+      const data = await response.json()
+      setTodos(data)
+    }
+    
+    getTodos()
   }, [])
 
   return (
@@ -25,6 +22,13 @@ function App() {
 
       <h1>ToDaLoos</h1>
       <div className="holder">
+        {todos.map(todo => {
+          return (
+            <>
+              <h2>{todo.task}</h2>
+            </>
+          )
+        })}
       </div>
 
     </>
